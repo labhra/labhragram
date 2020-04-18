ff22813570eeb8ce8f43f1ab3e411e6a09f17183
Laura <lauraduggan89@gmail.com>
2020-04-18 16:50:25 +0800

    works as blog

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Laura/Documents/Django SC/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ python_intro.py
@@ -0,0 +1,12 @@
+girls = ['rachel' , 'monica', 'sinead']
+
+
+def hi(name):
+    print("hi there! " + name)
+
+for name in girls:
+    hi(name)
+
+for i in range(3,6):
+    print(i)
+        
